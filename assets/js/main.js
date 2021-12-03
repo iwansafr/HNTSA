ae077cf2cf6316941c469f2762610ee89c6329b7
iwansafr <iwansafr@gmail.com>
2021-12-03 18:32:47 +0700

    revision

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,16 @@ $('#slickReels').slick({
   centerMode: true,
   centerPadding: '10px',
   slidesToShow: 3,
-  dots : true,
+  dots : false,
   responsive: [
     {
       breakpoint: 960,
       settings: {
-        arrows: false,
+        arrows: true,
         centerMode: true,
         centerPadding: '40px',
-        slidesToShow: 2
+        slidesToShow: 2,
+        dots: false
       }
     },
     {
@@ -24,3 +25,20 @@ $('#slickReels').slick({
     }
   ]
 });
+
+
+const playVideo = (el) => {
+  const videoTarget = el.parentElement.parentElement.querySelector('video');
+
+  if(el.querySelector('i').classList.contains('fa-play')){
+    videoTarget.play()
+    el.querySelector('i').classList.remove('fa-play')
+    el.querySelector('i').classList.add('fa-pause')
+  }
+  else{
+    el.querySelector('i').classList.add('fa-play')
+    el.querySelector('i').classList.remove('fa-pause')
+    videoTarget.pause()
+  }
+  
+}
